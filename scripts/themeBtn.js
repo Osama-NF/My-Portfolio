ec71d1453de6ebce56997c6d329fc7b1a5cea20b
Osama-NF <orz14322@gmail.com>
2022-11-24 16:02:03 +0300

    add contact + more coloring

---
+++ scripts/themeBtn.js
@@ -0,0 +1,75 @@
+/* 
+Theme 1 (darkish)
+#242582      -- darker blue
+#2f2fa2      -- lighter blue
+#f64c72      -- red orange thing
+#553d67      -- darker brown
+#99738e      -- lighter brown
+
+Theme 2 (lightish)
+#8d8741
+#659dbd
+#daad86
+#bc986a
+#fbeec1
+*/
+const THEMES = {
+    darkTheme: {
+        header: {
+            bg: "#242582",
+            text: "#eee"
+        },
+        main: {
+            bg: "#2f2fa2",
+            text: "#eee"
+        },
+        btn: {
+            bg: "#99738e",
+            text: "#000",
+            border: "#2f2fa2",
+            shadow: "#000"
+        },
+        pages: {
+            inner: {
+                bg: "#553d67",
+                text: "#eee"
+            },
+            outer: {
+                bg: "#99738e",
+                text: "#000"
+            },
+            border: "#000",
+            shadow: "#000"
+        }
+    },
+
+    lightTheme: {
+        header: {
+            bg: "#242582",
+            text: "#eee"
+        },
+        main: {
+            bg: "#2f2fa2",
+            text: "#eee"
+        },
+        btn: {
+            bg: "#99738e",
+            text: "#000",
+            border: "#2f2fa2",
+            shadow: "#000"
+        },
+        pages: {
+            inner: {
+                bg: "#553d67",
+                text: "#eee"
+            },
+            outer: {
+                bg: "#99738e",
+                text: "#000"
+            },
+            border: "#000",
+            shadow: "#000"
+        }
+    }
+
+}
