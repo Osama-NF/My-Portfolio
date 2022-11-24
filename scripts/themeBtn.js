d2cc735984e765086aabe8e399807d278f40af90
Osama-NF <orz14322@gmail.com>
2022-11-24 17:09:13 +0300

    finsihed making the basic theme button

--- scripts/themeBtn.js
+++ scripts/themeBtn.js
@@ -13,8 +13,8 @@ Theme 2 (lightish)
 #bc986a
 #fbeec1
 */
-const THEMES = {
-    darkTheme: {
+const THEMES = [
+    darkTheme = {
         header: {
             bg: "#242582",
             text: "#eee"
@@ -27,9 +27,11 @@ const THEMES = {
             bg: "#99738e",
             text: "#000",
             border: "#2f2fa2",
-            shadow: "#000"
+            shadow: "#f64c72"
         },
         pages: {
+            border: "#000",
+            shadow: "#f64c72",
             inner: {
                 bg: "#553d67",
                 text: "#eee"
@@ -37,39 +39,64 @@ const THEMES = {
             outer: {
                 bg: "#99738e",
                 text: "#000"
-            },
-            border: "#000",
-            shadow: "#000"
+            }
         }
     },
 
-    lightTheme: {
+    lightTheme = {
         header: {
-            bg: "#242582",
-            text: "#eee"
+            bg: "#659dbd",
+            text: "#000"
         },
         main: {
-            bg: "#2f2fa2",
-            text: "#eee"
+            bg: "#daad86",
+            text: "#000"
         },
         btn: {
-            bg: "#99738e",
+            bg: "#bc986a",
             text: "#000",
-            border: "#2f2fa2",
+            border: "#000",
             shadow: "#000"
         },
         pages: {
+            border: "#000",
+            shadow: "#000",
             inner: {
-                bg: "#553d67",
-                text: "#eee"
-            },
-            outer: {
-                bg: "#99738e",
+                bg: "#daad86",
                 text: "#000"
             },
-            border: "#000",
-            shadow: "#000"
+            outer: {
+                bg: "#bc986a",
+                text: "#000"
+            },
         }
     }
 
+]
+
+$('.theme-btn').click(function() {
+    previousIndex = Number($('body').attr('data-theme'))
+    newIndex = THEMES[previousIndex + 1] ? previousIndex + 1 : 0
+    $('body').attr('data-theme', newIndex)
+    
+    newTheme = THEMES[newIndex]
+    changeTheme(newTheme)
+})
+
+function changeTheme(newTheme) {
+    $(':root')
+        .css('--header-bg', newTheme.header.bg)
+        .css('--header-text', newTheme.header.text)
+        .css('--main-bg', newTheme.main.bg)
+        .css('--main-text', newTheme.main.text)
+        .css('--btn-bg', newTheme.btn.bg)
+        .css('--btn-text', newTheme.btn.text)
+        .css('--btn-border', newTheme.btn.border)
+        .css('--btn-shadow', newTheme.btn.shadow)
+        .css('--pages-border', newTheme.pages.border)
+        .css('--pages-shadow', newTheme.pages.shadow)
+        .css('--inner-bg', newTheme.pages.inner.bg)
+        .css('--inner-text', newTheme.pages.inner.text)
+        .css('--outer-bg', newTheme.pages.outer.bg)
+        .css('--outer-text', newTheme.pages.outer.text)
 }
